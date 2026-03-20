503b2c3535d355f93b7f5577f47b5fc5f2089aeb
Kenneth Ong <kenneth.ong3003@gmail.com>
2026-03-20 16:13:32 +0800

    Refactor JsonSerializableAddressBook

--- src/main/java/doctorwho/storage/JsonSerializableAddressBook.java
+++ src/main/java/doctorwho/storage/JsonSerializableAddressBook.java
@@ -19,16 +19,20 @@ import doctorwho.model.patient.Patient;
 @JsonRootName(value = "addressbook")
 class JsonSerializableAddressBook {
 
-    public static final String MESSAGE_DUPLICATE_PERSON = "Persons list contains duplicate patient(s).";
+    public static final String MESSAGE_DUPLICATE_PATIENT = "Patients list contains duplicate patient(s).";
 
-    private final List<JsonAdaptedPatient> persons = new ArrayList<>();
+    private final List<JsonAdaptedPatient> patients = new ArrayList<>();
 
     /**
-     * Constructs a {@code JsonSerializableAddressBook} with the given persons.
+     * Constructs a {@code JsonSerializableAddressBook} with the given patients.
      */
     @JsonCreator
-    public JsonSerializableAddressBook(@JsonProperty("persons") List<JsonAdaptedPatient> persons) {
-        this.persons.addAll(persons);
+    public JsonSerializableAddressBook(@JsonProperty("patients") List<JsonAdaptedPatient> patients,
+                                       @JsonProperty("patients") List<JsonAdaptedPatient> patient_2) {
+        List<JsonAdaptedPatient> patientsToLoad = patients != null ? patients : patient_2;
+        if (patientsToLoad != null) {
+            this.patients.addAll(patientsToLoad);
+        }
     }
 
     /**
@@ -37,7 +41,7 @@ class JsonSerializableAddressBook {
      * @param source future changes to this will not affect the created {@code JsonSerializableAddressBook}.
      */
     public JsonSerializableAddressBook(ReadOnlyAddressBook source) {
-        persons.addAll(source.getPatientList().stream().map(JsonAdaptedPatient::new).collect(Collectors.toList()));
+        patients.addAll(source.getPatientList().stream().map(JsonAdaptedPatient::new).collect(Collectors.toList()));
     }
 
     /**
@@ -47,10 +51,10 @@ class JsonSerializableAddressBook {
      */
     public AddressBook toModelType() throws IllegalValueException {
         AddressBook addressBook = new AddressBook();
-        for (JsonAdaptedPatient jsonAdaptedPatient : persons) {
+        for (JsonAdaptedPatient jsonAdaptedPatient : patients) {
             Patient patient = jsonAdaptedPatient.toModelType();
             if (addressBook.hasPatient(patient)) {
-                throw new IllegalValueException(MESSAGE_DUPLICATE_PERSON);
+                throw new IllegalValueException(MESSAGE_DUPLICATE_PATIENT);
             }
             addressBook.addPatient(patient);
         }
